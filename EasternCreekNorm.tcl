6386d5fe23eac40fb5a3d04f4da7b9c42983222a
Gergo Kaszas <gergo.kaszas@outlook.com>
2024-01-29 14:29:26 +1100

    Initial commit

---
+++ EasternCreekNorm.tcl
@@ -0,0 +1,256 @@
+x = 90.4716
+z = 7.62178
+angle = 175.807
+numsegment = 28
+segment0
+{
+	type = 0
+	length = 97.463402
+	radius = 0.000000
+	angle = 0.000000
+	height = -0.004501
+	heightlock = 0
+}
+segment1
+{
+	type = 1
+	length = 23.522697
+	radius = -20.922247
+	angle = 64.417137
+	height = -0.004498
+	heightlock = 0
+}
+segment2
+{
+	type = 0
+	length = 32.099342
+	radius = 100.000000
+	angle = 0.000000
+	height = -0.004496
+	heightlock = 0
+}
+segment3
+{
+	type = 1
+	length = 29.807182
+	radius = -13.918984
+	angle = 122.697586
+	height = -0.004499
+	heightlock = 0
+}
+segment4
+{
+	type = 0
+	length = 27.659817
+	radius = 100.000000
+	angle = 0.000000
+	height = -0.004503
+	heightlock = 0
+}
+segment5
+{
+	type = 1
+	length = 11.382652
+	radius = -9.401380
+	angle = 69.370453
+	height = -0.004504
+	heightlock = 0
+}
+segment6
+{
+	type = 1
+	length = 27.606258
+	radius = 20.764219
+	angle = 76.175369
+	height = -0.004507
+	heightlock = 0
+}
+segment7
+{
+	type = 0
+	length = 87.729050
+	radius = 100.000000
+	angle = 0.000000
+	height = -0.004521
+	heightlock = 0
+}
+segment8
+{
+	type = 1
+	length = 13.496261
+	radius = -8.954608
+	angle = 86.355400
+	height = -0.004523
+	heightlock = 0
+}
+segment9
+{
+	type = 0
+	length = 45.032726
+	radius = 100.000000
+	angle = 0.000000
+	height = -0.004523
+	heightlock = 0
+}
+segment10
+{
+	type = 1
+	length = 16.883352
+	radius = 10.862433
+	angle = 89.054153
+	height = -0.004524
+	heightlock = 0
+}
+segment11
+{
+	type = 1
+	length = 16.392887
+	radius = -10.185605
+	angle = 92.212814
+	height = -0.004526
+	heightlock = 0
+}
+segment12
+{
+	type = 1
+	length = 25.710768
+	radius = -23.727711
+	angle = 62.084309
+	height = -0.004524
+	heightlock = 0
+}
+segment13
+{
+	type = 1
+	length = 24.830870
+	radius = 22.495220
+	angle = 63.244724
+	height = -0.004522
+	heightlock = 0
+}
+segment14
+{
+	type = 0
+	length = 48.810246
+	radius = 100.000000
+	angle = 0.000000
+	height = -0.004521
+	heightlock = 0
+}
+segment15
+{
+	type = 1
+	length = 23.183681
+	radius = 26.744524
+	angle = 49.667252
+	height = -0.004523
+	heightlock = 0
+}
+segment16
+{
+	type = 1
+	length = 60.613430
+	radius = 194.136124
+	angle = 17.888962
+	height = -0.004531
+	heightlock = 0
+}
+segment17
+{
+	type = 1
+	length = 16.293486
+	radius = -9.171895
+	angle = 101.783546
+	height = -0.004531
+	heightlock = 0
+}
+segment18
+{
+	type = 1
+	length = 18.765402
+	radius = -84.063797
+	angle = 12.790028
+	height = -0.004529
+	heightlock = 0
+}
+segment19
+{
+	type = 1
+	length = 10.246378
+	radius = -11.110912
+	angle = 52.837624
+	height = -0.004528
+	heightlock = 0
+}
+segment20
+{
+	type = 1
+	length = 35.619492
+	radius = -31.880976
+	angle = 64.014557
+	height = -0.004524
+	heightlock = 0
+}
+segment21
+{
+	type = 1
+	length = 41.626125
+	radius = 31.171062
+	angle = 76.513313
+	height = -0.004519
+	heightlock = 0
+}
+segment22
+{
+	type = 1
+	length = 33.557812
+	radius = -21.167646
+	angle = 90.833008
+	height = -0.004515
+	heightlock = 0
+}
+segment23
+{
+	type = 0
+	length = 70.681549
+	radius = 100.000000
+	angle = 0.000000
+	height = -0.004516
+	heightlock = 0
+}
+segment24
+{
+	type = 1
+	length = 20.605404
+	radius = -19.285646
+	angle = 61.216652
+	height = -0.004517
+	heightlock = 0
+}
+segment25
+{
+	type = 1
+	length = 17.565266
+	radius = 17.409313
+	angle = 57.809032
+	height = -0.004519
+	heightlock = 0
+}
+segment26
+{
+	type = 0
+	length = 25.999557
+	radius = 100.000000
+	angle = 0.000000
+	height = -0.004519
+	heightlock = 0
+}
+segment27
+{
+	type = 1
+	length = 26.984921
+	radius = 19.656210
+	angle = 78.658195
+	height = -0.004517
+	heightlock = 0
+}
